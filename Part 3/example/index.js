a36917daeac0f2cec8b0cbffd323e771f3516557
tuan anh <tuananhbui0703@gmail.com>
2025-10-03 14:56:10 +0300

    to

--- Part 3/example/index.js
+++ Part 3/example/index.js
@@ -11,23 +11,7 @@ app.use(express.static('dist'))
 const PORT = process.env.PORT 
 
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+let notes = []
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
