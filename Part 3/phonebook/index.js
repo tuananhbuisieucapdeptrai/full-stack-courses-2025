f15a101660a12fbfd05f2f017aaa9ef96fc4d571
tuan anh <tuananhbui0703@gmail.com>
2025-10-03 11:50:41 +0300

    to

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body || {}));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 app.use(cors())
+app.use(express.static('dist'))
 
 
 
